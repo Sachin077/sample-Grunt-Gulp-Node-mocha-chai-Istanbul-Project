afae21325ef178630a8b48ecb051f2bb2254cecf
Sachin Aggarwal <t-saagg@microsoft.com>
2016-06-03 16:05:44 +0530

    added gulp-istanbul

--- GulpFile.js
+++ GulpFile.js
@@ -1,25 +1,41 @@
 var gulp = require('gulp');
 var fs = require('fs');
 var path = require('path');
-var ts = require('gulp-typescript');
-var mc = require('gulp-mocha');
+var ts = require('gulp-tsc');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 
-gulp.task('build', function() {
-  console.log('Compiling typescript');
-  return gulp.src(['node/*.ts'])
-    .pipe(ts({module: 'commonjs'})).js.pipe(gulp.dest('./js'))
+gulp.task('build', function(){
+  gulp.src(['node/*.ts'])
+    .pipe(ts({ sourceMap: true, declaration: true, outDir: './js' }))
+    .pipe(gulp.dest('./js'))
 });
 
-gulp.task('buildTest', function() {
-  console.log('Compiling typescript');
-  return gulp.src(['test/*.ts'])
-    .pipe(ts({module: 'commonjs'})).js.pipe(gulp.dest('./js-test1'))
+gulp.task('buildTest', function(){
+  gulp.src(['test/*.ts'])
+    .pipe(ts({ sourceMap: true, declaration: true, outDir: './js-test1' }))
+    .pipe(gulp.dest('./js-test1'))
 });
 
 gulp.task('testit', function () {
-  return gulp.src(['js-test1/**/*.js'], {read: false})
+  return gulp.src(['js-test1/test/*.js'], {read: false})
     // gulp-mocha needs filepaths so you can't have any plugins before it 
-    .pipe(mc({reporter: 'nyan'}));
+    .pipe(mocha({reporter: 'node_modules/mocha-junit-reporter'}));
 });
 
 gulp.task('test', ['buildTest', 'testit'])
+
+gulp.task('pre-test', function () {
+  return gulp.src(['js-test1/node/*.js'])
+    // Covering files 
+    .pipe(istanbul({includeUntested: true}))
+    // Force `require` to return covered files 
+    .pipe(istanbul.hookRequire());
+});
+ 
+gulp.task('coverage', ['pre-test'], function () {
+  return gulp.src(['js-test1/test/*.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran 
+    .pipe(istanbul.writeReports({reporters: ['cobertura']}));
+});
